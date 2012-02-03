9895775f5486183a289ecab27d116841fc56d967
Victor Hugo <victorvhpg@gmail.com>
2012-02-03 05:42:02 +0000

    primeira versao

--- jquery.trocaImg.js
+++ jquery.trocaImg.js
@@ -1,5 +1,28 @@
+/**
+ 
+ efeito que parece um flip horizontal
+ funciona assim:
+ anime a imagem para largura 0 e margin-left para a metade da largura,
+ apos o fim desta primeira animacao  
+ troque o src da imagem para outra imagem
+ e anime novamente para largura original e margin-left 0
+ Com isso parece que ela faz um flip mas na verdade nao faz :)
+ 
+ 
+ 
+ exemplo de uso:
+    $("img").trocaImg("imagens/teste.jpg");
+ 
+ */
+
+
+
+
+
+
+
 (function($) {
-    $.fn.trocaImg= function(srcB, configuracoes) {
+    $.fn.trocaImg = function(srcB, configuracoes) {
         var config = {
             "velocidade": 300
         };
